030a536f77c5ac4c500534324bbbe8c541116946
nayjay91 <40899722+nayjay91@users.noreply.github.com>
2021-12-05 22:30:43 -0500

    created a second layout option

--- Data Visualizations/Final_Project_East_6_App/app.R
+++ Data Visualizations/Final_Project_East_6_App/app.R
@@ -10,9 +10,8 @@ ui <- fluidPage(
     # Application title
     titlePanel("Moving South Bend Forward"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
+    
+    headerPanel(
             selectInput(inputId = "district",
                         "Council Disctrict:",
                         choices = c("Tim Scott",
@@ -22,20 +21,42 @@ ui <- fluidPage(
                                     "Dr. David Varner",
                                     "Oliver Davis",
                                     "All"
-                                    ),#this may be fine, but for programing would not want to hard code
+                        ),#this may be fine, but for programming would not want to hard code
                         selected = "All"
-                        )
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            tabsetPanel(type = "tabs",
-                        tabPanel("Demographics", verbatimTextOutput("demographics")),
-                        tabPanel("Schools and Businesses", verbatimTextOutput("ammenities")),
-                        tabPanel("Map", verbatimTextOutput("maps"))
             )
+        ),
+    mainPanel(
+        tabsetPanel(type = "tabs",
+                    tabPanel("demographics", verbatimTextOutput("demographics")),
+                    tabPanel("schools and businesses", verbatimTextOutput("ammenities")),
+                    tabPanel("map", verbatimTextOutput("maps"))
         )
     )
+    
+    ## second layout option (highlight + ctrl shift C to uncomment)
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         selectInput(inputId = "district",
+    #                     "Council Disctrict:",
+    #                     choices = c("Tim Scott",
+    #                                 "Regina Williams",
+    #                                 "Sharon McBride",
+    #                                 "Jo M. Broden",
+    #                                 "Dr. David Varner",
+    #                                 "Oliver Davis",
+    #                                 "All"
+    #                     ),#this may be fine, but for programming would not want to hard code
+    #                     selected = "All"
+    #                     )
+    #     ),
+    #     mainPanel(
+    #         tabsetPanel(type = "tabs",
+    #                     tabPanel("demographics", verbatimTextOutput("demographics")),
+    #                     tabPanel("schools and businesses", verbatimTextOutput("ammenities")),
+    #                     tabPanel("map", verbatimTextOutput("maps"))
+    #         )
+    #     )
+    # )
 )
 
 # Define server logic required to draw a histogram
