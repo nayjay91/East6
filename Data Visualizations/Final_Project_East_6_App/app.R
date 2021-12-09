2aa69061b5fb02e80bc2f1eada00259f750ed63d
ebschultz <26803933+ebschultz@users.noreply.github.com>
2021-12-08 22:00:36 -0500

    start of part B

--- Data Visualizations/Final_Project_East_6_App/app.R
+++ Data Visualizations/Final_Project_East_6_App/app.R
@@ -82,6 +82,7 @@ server <- function(input, output, session) {
     
     output$map <- renderLeaflet({
         ## TODO: 
+        ## [] Population density layer (& fix All polygons)
         ## [] Fix colors... add symbols? 
         ## [] Filter by types?? separate layer controls?
         
